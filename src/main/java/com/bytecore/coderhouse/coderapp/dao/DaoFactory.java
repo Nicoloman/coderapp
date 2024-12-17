4e54bbaf4a21d87bbaa987e7e94bb69abef8fc1d
niqloma <nicolomanto@gmail.com>
2024-12-17 11:54:43 -0300

    Dao factory

--- src/main/java/com/bytecore/coderhouse/coderapp/dao/DaoFactory.java
+++ src/main/java/com/bytecore/coderhouse/coderapp/dao/DaoFactory.java
@@ -3,6 +3,7 @@ package com.bytecore.coderhouse.coderapp.dao;
 import org.springframework.stereotype.Service;
 
 import com.bytecore.coderhouse.coderapp.entities.Alumno;
+import com.bytecore.coderhouse.coderapp.entities.Categoria;
 import com.bytecore.coderhouse.coderapp.entities.Curso;
 
 import jakarta.persistence.EntityManager;
@@ -23,5 +24,9 @@ public class DaoFactory {
 	@Transactional
 	public void persistirCurso(Curso curso) {
 		em.persist(curso);
+	}
+	@Transactional
+	public void persistirCategoria(Categoria categoria) {
+		em.persist(categoria);
 	}
 }
